c03b8beab571c4672c3475ff68429af06d1ccca1
makkenzo <nekgo2009@gmail.com>
2024-12-25 10:55:14 +0500

    Enhances Tauri app with greeting feature
    Updates app description and author in Cargo.toml
    Modifies Rust greeting function for consistency
    Adds React component to invoke Rust function and display greeting
    
    Improves front-end and back-end integration

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,26 @@
+'use client';
+
+import { invoke } from '@tauri-apps/api/core';
+import { useEffect, useState } from 'react';
+
 export default function Page() {
-    const asd = 'asd';
-    return <h1>Hello, Next.js!</h1>;
+    const [greet, setGreet] = useState('');
+
+    useEffect(() => {
+        const invokeRust = async () => {
+            await invoke('greet', { name: 'World' }).then((res) => {
+                setGreet(res as string);
+            });
+        };
+
+        invokeRust();
+    }, []);
+
+    return (
+        <div className="flex flex-col p-8">
+            <h1>Hello, Next.js!</h1>
+
+            {greet ? greet : null}
+        </div>
+    );
 }
