dc589e2d7e1b9feb5b5d26879953161b49ac5bf4
makkenzo <nekgo2009@gmail.com>
2024-12-25 11:11:50 +0500

    Adds async custom command and updates dependencies
    Introduces a new asynchronous custom command `my_custom_command` to handle database interactions and returns a structured response
    Adds `tokio` dependency for asynchronous support
    Updates the front-end to invoke the new custom command and display the result

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,3 +1,33 @@
+struct Database;
+
+#[derive(serde::Serialize)]
+struct CustomResponse {
+    message: String,
+    other_val: usize,
+}
+
+async fn some_other_function() -> Option<String> {
+    Some("response".into())
+}
+
+#[tauri::command]
+async fn my_custom_command(
+    window: tauri::Window,
+    number: usize,
+    database: tauri::State<'_, Database>,
+) -> Result<CustomResponse, String> {
+    println!("Called from {}", window.label());
+    let result: Option<String> = some_other_function().await;
+    if let Some(message) = result {
+        Ok(CustomResponse {
+            message,
+            other_val: 42 + number,
+        })
+    } else {
+        Err("No result".into())
+    }
+}
+
 // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
 #[tauri::command]
 fn greet(name: &str) -> String {
@@ -7,8 +37,9 @@ fn greet(name: &str) -> String {
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
+        .manage(Database {})
         .plugin(tauri_plugin_opener::init())
-        .invoke_handler(tauri::generate_handler![greet])
+        .invoke_handler(tauri::generate_handler![greet, my_custom_command])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,12 +5,17 @@ import { useEffect, useState } from 'react';
 
 export default function Page() {
     const [greet, setGreet] = useState('');
+    const [message, setMessage] = useState('');
 
     useEffect(() => {
         const invokeRust = async () => {
             await invoke('greet', { name: 'World' }).then((res) => {
                 setGreet(res as string);
             });
+
+            await invoke('my_custom_command', { number: 42 })
+                .then((res: any) => setMessage(`Message: ${res.message}, Other Val: ${res.other_val}`))
+                .catch((e) => console.error(e));
         };
 
         invokeRust();
@@ -20,7 +25,8 @@ export default function Page() {
         <div className="flex flex-col p-8">
             <h1>Hello, Next.js!</h1>
 
-            {greet ? greet : null}
+            {greet ? <p>{greet}</p> : null}
+            {message ? <p>{message}</p> : null}
         </div>
     );
 }
