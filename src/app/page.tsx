05042a08d225bff694a61172f8ac56452ab1892f
makkenzo <nekgo2009@gmail.com>
2024-12-25 10:30:55 +0500

    tauri + next init

---
+++ src/app/page.tsx
@@ -0,0 +1,4 @@
+export default function Page() {
+    const asd = 'asd';
+    return <h1>Hello, Next.js!</h1>;
+}
---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: ['./components/**/*.{js,ts,jsx,tsx,mdx}', './src/**/*.{js,ts,jsx,tsx,mdx}'],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+};
